79eddcdc361e0b339225b371cbc797c31f5b6a6b
Ichsan Firdaus <ichsanfirdaus99@gmail.com>
2016-05-11 10:40:23 +0700

    Country, Currency, and Language using Materialize

--- resources/views/master/country.blade.php
+++ resources/views/master/country.blade.php
@@ -1,1 +1,43 @@
-@extends('app')@section('title')Country @endsection @section('content') <section class="main-content"> <div class="content-wrap"> <div class="wrapper"> <div class="row"> <div class="col-md-12"> <section class="panel no-b"> <header class="panel-heading no-b"> <h2><b>Country</b></h2> </header> <div class="panel-body"> <div class="table-responsive"> <table class="table table-striped table-bordered responsive no-m" data-sortable="" data-sortable-initialized="true"> <thead> <tr> <th style="width: 30px;">No</th> <th>Country Id</th> <th>Country Name</th> <th>Country Area Code</th> </tr> </thead> <tbody> <?php $no = 1; ?> @foreach($data as $key) <tr> <td>{{$no++}}</td> <td>{{$key->country_id}}</td> <td>{{$key->country_name}}</td> <td>{{$key->country_areacode}}</td> </tr> @endforeach </tbody> </table> </div> </div> </section> </div> </div> </div> </div> <a class="exit-offscreen"></a> </section>@endsection
+@extends('app')@section('title')Country @endsection @section('content') <section class="main-content">
+
+<div class="content-wrap">
+
+<div class="wrapper">
+
+<div class="row">
+
+<div class="col-md-12"> <section class="panel no-b">
+
+<div class="panel-body">
+
+<div class="table-responsive">
+  <table class="striped">
+          <thead>
+            <tr>
+                <th>No</th>
+                <th>Country Id</th>
+                <th>Country Name</th>
+                <th>Country Area Code</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            @foreach($data as $key => $row)
+              <tr>
+                <td>{{$key+1}}
+                </td>
+                <td>{{$row->country_id}}
+                </td>
+                <td>{{$row->country_name}}
+                </td>
+                <td>{{$row->country_areacode}}
+                </td>
+              </tr> @endforeach
+          </tbody>
+        </table>
+</div>
+</div> </section>
+</div>
+</div>
+</div>
+</div> <a class="exit-offscreen"></a> </section>@endsection
