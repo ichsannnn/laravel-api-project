18292425d985c64bce24240fa5e9db0e98829d9d
Ichsan Firdaus <ichsanfirdaus99@gmail.com>
2016-05-11 10:39:16 +0700

    Fix Template

--- resources/views/app_flight.blade.php
+++ resources/views/app_flight.blade.php
@@ -3,34 +3,27 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
   <title>@yield('title')</title>
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="{{asset('assets/css/materialize.css')}}" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="{{asset('assets/css/style.css')}}" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <script src="{{asset('assets/js/jquery.min.js')}}"></script>
+  <script type="text/javascript">
+  // $.ajaxSetup({
+  //   headers: {
+  //     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+  //   }
+  // });
+  </script>
 </head>
 <body>
-  <ul id="master" class="dropdown-content">
-    <li><a href="{{url('master/currency')}}">Currency</a></li>
-    <li><a href="{{url('master/country')}}">Country</a></li>
-    <li><a href="{{url('master/language')}}">Language</a></li>
-    <li><a href="{{url('master/airport')}}">Airport</a></li>
-  </ul>
-  <ul id="cron" class="dropdown-content">
-    <li><a href="{{url('cronGetCurrency')}}">Currency</a></li>
-    <li><a href="{{url('cronGetCountry')}}">Country</a></li>
-    <li><a href="{{url('cronGetLanguage')}}">Language</a></li>
-    <li><a href="{{url('cronGetAirport')}}">Airport</a></li>
-  </ul>
 
   <nav class="light-blue lighten-1" role="navigation">
     <div class="nav-wrapper container">
-      <a id="logo-container" href="{{url('master/country')}}" class="brand-logo">Flight<b>Airline</b></a>
-      <ul class="right hide-on-med-and-down">
-        <li><a class="dropdown-button" href="#!" data-activates="master">Master<i class="material-icons right">arrow_drop_down</i></a></li>
-        <li><a class="dropdown-button" href="#!" data-activates="cron">Cron<i class="material-icons right">arrow_drop_down</i></a></li>
-      </ul>
+      <a id="logo-container" href="{{url('airline/flight')}}" class="brand-logo">Flight<b>Airline</b></a>
     </div>
   </nav>
   <div class="section no-pad-bot" id="index-banner">
@@ -40,12 +33,10 @@
     </div>
   </div>
 
-  <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
   <script src="{{asset('assets/js/materialize.min.js')}}"></script>
   <script src="{{asset('assets/js/init.js')}}"></script>
   @yield('js')
-
   </body>
 </html>
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,8 +4,32 @@
         <a href="{{ url('dashboard') }}" class="navbar-brand"> <img src="{{ asset('assets/img') }}/wilogo.png" alt=""> <span class="heading-font">BelajarAPI</span> </a>
     </div>
     <ul class="nav navbar-nav navbar-right">
-        
+
         @if(Auth::check())
+          <li class="off-right"> <a href="javascript:;" data-toggle="dropdown"><span class="hidden-xs ml10">Currency</span> <i class="ti-angle-down ti-caret hidden-xs"></i> </a>
+            <ul class="dropdown-menu animated fadeInRight">
+              <li> <a href="{{ url('cronGetCurrency') }}">Get Data</a> </li>
+              <li> <a href="{{ url('master/currency') }}">View Data</a> </li>
+            </ul>
+          </li>
+          <li class="off-right"> <a href="javascript:;" data-toggle="dropdown"><span class="hidden-xs ml10">Language</span> <i class="ti-angle-down ti-caret hidden-xs"></i> </a>
+            <ul class="dropdown-menu animated fadeInRight">
+              <li> <a href="{{ url('cronGetLanguage') }}">Get Data</a> </li>
+              <li> <a href="{{ url('master/language') }}">View Data</a> </li>
+            </ul>
+          </li>
+          <li class="off-right"> <a href="javascript:;" data-toggle="dropdown"><span class="hidden-xs ml10">Country</span> <i class="ti-angle-down ti-caret hidden-xs"></i> </a>
+            <ul class="dropdown-menu animated fadeInRight">
+              <li> <a href="{{ url('cronGetCountry') }}">Get Data</a> </li>
+              <li> <a href="{{ url('master/country') }}">View Data</a> </li>
+            </ul>
+          </li>
+          <li class="off-right"> <a href="javascript:;" data-toggle="dropdown"><span class="hidden-xs ml10">Airport</span> <i class="ti-angle-down ti-caret hidden-xs"></i> </a>
+            <ul class="dropdown-menu animated fadeInRight">
+              <li> <a href="{{ url('cronGetAirport') }}">Get Data</a> </li>
+              <li> <a href="{{ url('master/airport') }}">View Data</a> </li>
+            </ul>
+          </li>
           <li class="off-right"> <a href="javascript:;" data-toggle="dropdown"><img src="{{ asset('assets/img/profile/default.jpg') }}" class="header-avatar img-circle" alt="user" title="user"> <span class="hidden-xs ml10">@if(Auth::check()){{Auth::user()->name}}@endif</span> <i class="ti-angle-down ti-caret hidden-xs"></i> </a>
             <ul class="dropdown-menu animated fadeInRight">
               <li> <a href="{{ url('auth/logout') }}">Logout</a> </li>
